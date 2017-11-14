a72c56320870ef9b93892cf0834cce5b7e4dbe9c
Chihman <chihmanD2017@gmail.com>
2017-11-14 21:33:29 +0700

    лаб 5 Задание 30-31

--- Лабораторная 5/Лабораторная 5/Задание 5-31/Program.cs
+++ Лабораторная 5/Лабораторная 5/Задание 5-31/Program.cs
@@ -9,7 +9,53 @@ namespace Задание_5_31
     class Program
     {
         static void Main(string[] args)
-        {////
+        {
+            Console.WriteLine("Введите данные A1: ");
+            double A1 = Double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите данные B1: ");
+            double B1 = Double.Parse(Console.ReadLine());
+
+            if (A1 > B1)
+            {
+                Console.WriteLine(" Значение A должно быть меньше значения B ");
+                return;
+            }
+            Console.WriteLine("Введите данные A2: ");
+            double A2 = Double.Parse(Console.ReadLine());
+           
+            Console.WriteLine("Введите данные B2: ");
+            double B2 = Double.Parse(Console.ReadLine());
+
+            if (A2>B2)
+            {
+                Console.WriteLine(" Значение A должно быть меньше значения B ");
+                return;
+            }
+            
+            double A,B;
+            if (A1 > A2)
+            { A = A1; }
+            else { A = A2; }
+
+            if (B1 < B2)
+            { B = B1; }
+            else { B = B2; }
+
+            if (B1<A2 || B2<A1 )
+            {
+                Console.WriteLine(" не пересекаются ");
+                return;
+            }
+          
+            if (B2>=A1 || B1>=A2)
+            {                               
+                Console.WriteLine(" Пересекаются  ({0:F0};{1:F0})", A , B);
+                            }
+            
+            
+
+
         }
     }
 }
---
+++ Лабораторная 5/Лабораторная 5/Задание 5-32/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные X1: ");
+            double X1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите данные Y1: ");
+            double Y1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите данные H1: ");
+            double H1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите данные W1: ");
+            double W1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите данные X2: ");
+            double X2 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите данные Y2: ");
+            double Y2 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите данные H2: ");
+            double H2 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите данные W2: ");
+            double W2 = Double.Parse(Console.ReadLine());
+            double X, Y, XX, YY, A, B, W3, Y3;
+            X = X1 + W1;
+            Y = Y1 + H1;
+            XX = X2 + W2;
+            YY = Y2 + H2;
+
+            //ширина
+            X = X1 + W1;
+            XX = X2 + W2;
+
+            if (XX <= X)
+            {
+                W3 = XX;
+            }
+            else { W3 = X; }
+
+            if (X1 >= X2 || Y1 >= Y2)
+            {
+                //ПЕРВАЯ ЛЕВАЯ ТОЧКА
+                A = X1;
+                B = Y1;
+                W3 = X2;
+
+            }
+            else
+            { A = X2;
+                B = Y2;
+            }
+
+           
+
+            
+
+            //высота
+            Y = Y1 + H1;
+            YY = Y2 + H2;
+
+            if (YY <= Y)
+            { Y3 = YY; }
+            else { Y3 = Y; }
+
+
+            Console.WriteLine("\"x:\" {0:F0},\"y:\" {1:F0},\"w:\"{2:F0},\"h:\"{3:F0}", A, B, W3, Y3);
+
+        
+    
+        }
+    }
+}
