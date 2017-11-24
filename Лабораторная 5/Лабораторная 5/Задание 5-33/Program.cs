b79819b6487088389fc011cee7213ad356952885
chihman <chihmanD2017@gmail.com>
2017-11-24 16:12:22 +0600

    Лаб 5 Задания 33,34

---
+++ Лабораторная 5/Лабораторная 5/Задание 5-33/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Введите данные t: ");
+            double t = Double.Parse(Console.ReadLine());
+            if (t < 0 || t > 59)
+            {
+                Console.WriteLine("Значение T должно быть в интервале [0, 59]");
+                return;
+            }
+            if (t == 0)
+            {
+                Console.WriteLine("Зеленый");
+                return;
+            }
+
+            double X;
+            X = 0;
+            X = t % 10;
+            if (X == 1 || X == 2 || X == 5 || X == 6 || X == 7 || X == 8)
+            {
+                Console.WriteLine("Зеленый");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Красный");
+            }
+
+
+        }
+    }
+}
---
+++ Лабораторная 5/Лабораторная 5/Задание 5-34/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5_34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите данные W: ");
+            double w = Double.Parse(Console.ReadLine());
+            if (w<1)
+            {
+                Console.WriteLine("Значение W должно быть положительным");
+                return;
+            }
+
+            Console.WriteLine("Введите данные h: ");
+            double h = Double.Parse(Console.ReadLine());
+            if (h < 1)
+            {
+                Console.WriteLine("Значение h должно быть положительным");
+                return;
+            }
+
+            Console.WriteLine("Введите данные C: ");
+            double c = Double.Parse(Console.ReadLine());
+            if (c < 1)
+            {
+                Console.WriteLine("Значение C должно быть положительным");
+                return;
+            }
+
+            Console.WriteLine("Введите данные D: ");
+            double d = Double.Parse(Console.ReadLine());
+            if (d < 1)
+            {
+                Console.WriteLine("Значение D должно быть положительным");
+                return;
+            }
+            double X1, X2,Y1,Y2,B, A;
+           
+
+            X1 = (int)(h / c);
+            X2 = (int)(w / d);
+            A = (X1 * X2);
+
+            Y1 = (int)(h / d);
+            Y2 = (int)(w / c);
+            B = (Y1 * Y2);
+
+            if (A>B)
+            {
+                
+                Console.WriteLine(A+" при С вдоль Н ");
+            }
+
+            else
+            {
+               
+                Console.WriteLine(B + " при C вдоль W ");
+            }
+            
+        }
+    }
+}
