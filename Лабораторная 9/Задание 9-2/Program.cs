7898d264e90d178427f335c542ebd552bc3c4d65
chihman <chihmanD2017gmail.com>
2018-01-01 13:21:39 +0600

    9-2

---
+++ Лабораторная 9/Задание 9-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;//обязательная строка
+
+namespace Задание_9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (!File.Exists("..\\..\\test" + n + ".txt")) //проверяет наличие файла
+            {
+                Console.Write("oshibka fail ne sushestvuet");
+                return;
+            }
+            StreamReader reader = new StreamReader("..\\..\\test" + n + ".txt"); //читает файл
+            if (reader.EndOfStream)
+            {
+                Console.Write("Файл пуст");
+                return;
+            }
+            while (!reader.EndOfStream)
+            {
+                String line = reader.ReadLine(); //читает первую строку
+                Console.Write(line + " ");
+            }
+        }
+    }
+}
